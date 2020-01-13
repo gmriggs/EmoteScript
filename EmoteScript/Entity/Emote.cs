6a3a5526fee307c90cc214a535dfbfbd8ef695b7
gmriggs <gmriggs@gmail.com>
2020-01-13 04:31:52 -0500

    fix null pointer exception in GetMessageKey()

--- EmoteScript/Entity/Emote.cs
+++ EmoteScript/Entity/Emote.cs
@@ -220,7 +220,7 @@ namespace EmoteScript
 
             string key = Message;
 
-            if (key.Contains("@"))
+            if (key != null && key.Contains("@"))
                 return key;
 
             switch (Type)
